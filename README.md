ba8e53f68579cd380a0a30a9f944ff893cecff0b
Mashrukh Islam <mushroomgenie@Iceberg.local>
2025-07-08 00:07:10 +0530

    implement product fetcher

--- README.md
+++ README.md
@@ -1,36 +1,33 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+This is a Hiring Task for an AI x Commerce Startup
 
-## Getting Started
+- The task is to implement a generic product link fetching tool based on the user's country.
+- The tech stack here is NextJs and I am using Google's Gemini API to fetch the product information.
+- You can find the prompt in `src/page.tsx`
 
-First, run the development server:
+## Installing dependencies
+
+Run the following command to install dependencies:
+
+```bash
+npm install
+```
+- External library used:
+    - [react-json-view-lite](https://www.npmjs.com/package/react-json-view-lite)
+
+## Running the development server
+
+Run the development server with the following:
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Demo
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+The task stated that a demo be displayed in the repo. Below are two GIFs demonstrating the product link fetcher.
 
-## Learn More
+![Demo](demo/DemoRecording.gif)
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+![Demo1](demo/DemoRecording1.gif)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
-import Image from "next/image";
+"use client";
+import { JsonView } from "react-json-view-lite";
+import "react-json-view-lite/dist/index.css";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [country, setCountry] = useState("");
+  const [product, setProduct] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [productsData, setProductsData] = useState<
+    Array<{
+      link: string;
+      price: string;
+      currency: string;
+      productName: string;
+    }>
+  >([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const isDisabled = product === "" || country === "";
+
+  const fetchPrices = async () => {
+    setCountry("");
+    setProduct("");
+    setProductsData([]);
+    setIsLoading(true);
+    const payload = {
+      contents: [
+        {
+          parts: [
+            {
+              text: `Fetch links for ${product} in ${country} country. Give the response in below format and sort the prices in ascending order
+              "
+              {
+                "link":"",
+                "price":"",
+                "currency":"",
+                "productName":""
+              }
+              "
+              `,
+            },
+          ],
+        },
+      ],
+    };
+    const response = await fetch(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-goog-api-key": `${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`,
+        },
+        body: JSON.stringify(payload),
+      }
+    );
+    const responseJSON = await response.json()
+    console.log(responseJSON)
+    const finalData = JSON.parse(responseJSON.candidates[0].content.parts[0].text.split("json")[1].replaceAll("```",""))
+    setIsLoading(false)
+    setProductsData(finalData)
+  };
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] bg-white items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="flex flex-col align-items-center">
+        <label>Enter Country</label>
+        <input
+          type="text"
+          placeholder="Country"
+          value={country || ""}
+          onChange={(event) => setCountry(event.target.value)}
+        />
+        <label>Enter Product</label>
+        <input
+          type="text"
+          placeholder="Product"
+          value={product}
+          onChange={(event) => setProduct(event.target.value)}
+        />
+        <button
+          onClick={fetchPrices}
+          className={`${isDisabled ? "cursor-not-allowed" : "cursor-pointer"} ${
+            isDisabled ? "bg-red-100" : "bg-green-100"
+          } rounded-2xl mt-10`}
+          type="button"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Search
+        </button>
+      </div>
+      <div className="mt-10">
+        {isLoading ? (
+          <p> Loading ...</p>
+        ) : productsData.length === 0 ? null : (
+          <JsonView data={productsData} />
+        )}
+      </div>
     </div>
   );
 }
